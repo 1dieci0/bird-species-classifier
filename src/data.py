ff2f69bc440ca96ed5df45969d030792b72fd6e7
1dieci0 <pescesonnambulo@gmail.com>
2026-07-11 10:39:42 +0200

    fixed data implementation

--- src/data.py
+++ src/data.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from dataset.py import BirdDataset
+from dataset import BirdDataset
 from sklearn.model_selection import train_test_split
 
-def load_dataframe(dataset_path):
+def load_dataframe(DATASET_PATH):
     images = pd.read_csv(
         os.path.join(DATASET_PATH, "images.txt"),
         sep=" ",
@@ -59,8 +59,9 @@ def create_transforms():
         transforms.Resize((224,224)),
         transforms.ToTensor()
     ])
+    return train_transform, test_transform
 
-def create_train_val_test():
+def create_train_val_test(DATASET_PATH, train_df, val_df, test_df, train_transform, test_transform):
     train_dataset = BirdDataset(
         train_df,
         DATASET_PATH,
@@ -79,9 +80,13 @@ def create_train_val_test():
         transform=test_transform
     )
 
-def create_loaders():
+    return train_dataset, val_dataset, test_dataset
+
+def create_loaders(train_dataset, val_dataset, test_dataset):
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    return train_loader, val_loader, test_loader
 
 
